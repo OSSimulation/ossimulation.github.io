429fe7aaffa39e344f4483bd628e643b94f6df6e
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2024-10-04 10:09:26 +1000

    🫡8

--- winxp/old/news.js
+++ winxp/old/news.js
@@ -7,37 +7,40 @@ function changeNewsMsg(newsTitl,newsSubTitl,link) {
     newsSubTitle.setAttribute("href",link); // set the link
 }
 function newsLoop() {
-    changeNewsMsg("NEWS","Try out the new OS Menu","https://www.roblox.com/games/18140408049");
+    changeNewsMsg("NEWS","All of our Roblox games have been permamently taken down","#");
     setTimeout(() => {
-        changeNewsMsg("NEWS","Windows RT Gui Test has been released","https://www.roblox.com/games/17420222147");
+        changeNewsMsg("NEWS","Try out the new OS Menu","#");
     }, 5000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","Windows XP Gui Test was accidently set to 17+, whoops","#");
+        changeNewsMsg("NEWS","Windows RT Gui Test has been released","#");
     }, 10000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","iOS 6 finally gets its own page","../ios6/roblox/index.html");
+        changeNewsMsg("NEWS","Windows XP Gui Test was accidently set to 17+, whoops","#");
     }, 15000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","Windows XP Gui Test is now 3 years old","#");
+        changeNewsMsg("NEWS","iOS 6 finally gets its own page","../ios6/roblox/index.html");
     }, 20000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","A new update for Windows XP Gui Test has been released after almost a year of no updates","./updates/index.html");
+        changeNewsMsg("NEWS","Windows XP Gui Test is now 3 years old","#");
     }, 25000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","iOS 6 (on Roblox) is still dead","../ios6/roblox/index.html");
+        changeNewsMsg("NEWS","A new update for Windows XP Gui Test has been released after almost a year of no updates","./updates/index.html");
     }, 30000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","XBOX 360 Gui Test has been discontinued","../xbox360/index.html");
+        changeNewsMsg("NEWS","iOS 6 (on Roblox) is still dead","../ios6/roblox/index.html");
     }, 35000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","Thomas actually did something for this website","#");
+        changeNewsMsg("NEWS","XBOX 360 Gui Test has been discontinued","../xbox360/index.html");
     }, 40000);
     setTimeout(() => {
-        changeNewsMsg("NEWS","I'm out of funny messages, so this will loop","#");
+        changeNewsMsg("NEWS","Thomas actually did something for this website","#");
     }, 45000);
     setTimeout(() => {
-        newsLoop(); // restart the loop
+        changeNewsMsg("NEWS","I'm out of funny messages, so this will loop","#");
     }, 50000);
+    setTimeout(() => {
+        newsLoop(); // restart the loop
+    }, 55000);
 }
 
 window.addEventListener('load', function () {
